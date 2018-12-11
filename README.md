cacb7da29d88187d39f3b0cd6e7fd95b4f412101
augustomatheuss <augustomatheuss@ufrn.edu.br>
2018-12-10 23:20:26 -0300

    init the lib, added matrixAlloc, matrixFree and function as parameter

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # personalc
-My personal reference to C programming
+My personal reference to C programming  
+
+## Usage examples in file examples.c  
+* Compile 
+```
+gcc -lm examples.c personal.c -o examples
+```  
+* Run
+```
+./examples
+```
+
+## Check memory leaks in your program  
+```
+valgrind --leak-check=full --track-origins=yes name-of-your-program
+```
+
+## References:  
+* http://www.cplusplus.com/reference/clibrary/  
+
---
+++ examples.c
@@ -0,0 +1,72 @@
+/* Personal reference to C programming
+ *
+ * Copyright (c) 2018, Augusto Damasceno.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include "personal.h"
+
+
+float myfunction(float a, float b)
+{
+	return a + b;
+}
+
+
+int main(int argc, char ** argv)
+{
+	printf("Usage examples for personalc library.\n\n");
+
+	printf("Example for function ");
+	printf("double ** matrixAlloc(int r, int c)\n");
+	printf("r = 2 and c = 3\n\n");
+	double ** matrix = matrixAlloc(2,3);
+	srand (time(NULL));	/* Initialize random seed */
+	int i,j;
+	for (i=0; i<2; i++)
+	{
+		for (j=0; j<3; j++)
+		{
+			matrix[i][j] = ( (float) (rand() % 10000) ) / 100.0;
+			printf("%.2f  ",matrix[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	/* Free memory for matrix allocated */
+	printf("Free the memory allocated for the matrix.\n");
+	printf("matrixFree(matrix,2,3)\n\n");
+	matrixFree(matrix,2,3);
+
+	printf("Example for function ");
+	printf("void functionParameter(float(*fun)(float,float)\n");
+	printf("The function fun returns the sum of arguments\n");
+	functionParameter(myfunction);
+	printf("\n\n");	
+	
+	return 0;
+} 
+
