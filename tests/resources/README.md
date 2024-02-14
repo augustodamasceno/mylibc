f013ef60784c84871cb8aa5864da130b85defd7c
augustodamasceno <augustodamasceno@protonmail.com>
2024-02-14 17:50:54 -0300

    added math test with resource file for moving correlation

--- tests/resources/README.md
+++ tests/resources/README.md
@@ -50,6 +50,37 @@ if __name__ == "__main__":
   np.savetxt('sma-nan.csv', data, delimiter=',', header='Data, SMA', comments='', fmt='%.12f')
 ```
 
+# mcorr.csv  
+* Use Mersenne Twister as the pseudo-random number generator;  
+* Set seed to 42;  
+* Generate two columns with 100 numbers in the interval [0, 1);  
+* Calculates the moving correlation with window 5;  
+* Saves all 100 numbers for both columns and 96 iterations of the moving correlation with 12 decimal places. 
+* The moving correlation start with the current index. The last "window size-1" values are NAN. 
+
+## Code Sample
+```python
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+import numpy as np
+
+SEED = 42
+
+if __name__ == "__main__":
+  np.random.seed(SEED)
+  random_nums_a = np.random.rand(100) 
+  random_nums_b = np.random.rand(100) 
+  window_size = 5
+  mcoor = np.array([np.corrcoef(random_nums_a[i:i+window_size], random_nums_b[i:i+window_size])[0, 1] \
+                  for i in range(len(random_nums_a) - window_size + 1)])
+  nan_values = np.full(window_size - 1, np.nan) 
+  mcoor = np.concatenate((mcoor, nan_values))
+  data = np.column_stack((random_nums_a, random_nums_b, mcoor))
+  np.savetxt('mcorr.csv', data, delimiter=',', header='A, B, Moving Correlation A and B', comments='', fmt='%.12f')
+```
+
+
 # SHA1SUMS
 fbeb357843c725cfb5efdfe8548c1d41e7068441  sma-nan.csv
 5bb0905b8e5e21322af3907d7848bd876fb899dd  sma.csv
+87188a0ff07313b659f69a6d1605f8b2ca4ce07e  mcorr.csv
--- tests/test_mmcorr.c
+++ tests/test_mmcorr.c
@@ -7,11 +7,12 @@
  */
 
 #include <stdlib.h>
-/* sprintf, fprintf, stderr */
 #include <stdio.h>
 #include <math.h>
 
 #include "../src/mmcorr.h"
+#include "../src/msqueue.h"
+#include "../src/mcsv.h"
 #include "test_mmcorr.h"
 
 
@@ -84,12 +85,80 @@ START_TEST (testAddGet) {
 	mcorr_destruct(&mcorr);
 }
 
+START_TEST (testMCORRMath) {	
+	const double NUM_DECIMAL_PLACES = 10.0;
+	const uint64_t period = 5;
+	const uint64_t DATA_A_COLUMN = 0;
+	const uint64_t DATA_B_COLUMN = 1;
+	const uint64_t MCORR_COLUMN = 2;
+	const char DATA_FILENAME[23] = "resources/mcorr.csv";
+	const char SEPARATOR[2] = ",";	
+	uint64_t num_rows = 0;
+	uint64_t num_columns = 0;
+	uint64_t skip_rows = 1;
+	uint64_t index_row = 0;
+	double data_val = 0;
+	double mcorr_val = 0;
+	double mcorr_val_rounded = 0;
+	double data_val_rounded = 0;
+	double round_mult = pow(10, NUM_DECIMAL_PLACES);
+	double ** matrix;
+	MovingCorrelation * mcorr = mcorr_init(period);
+	StaticQueue * second_var = squeue_init(sizeof(double), period);
+	Table table;
+	read_csv(DATA_FILENAME, SEPARATOR, &table);
+
+	matrix = table_as_double(&table,
+							 skip_rows,
+							 &num_rows,
+							 &num_columns);
+    /*
+	for (index_row=0; index_row<num_rows; index_row++)
+		printf("%0.8f\t%0.8f\n", matrix[index_row][0], matrix[index_row][1]);
+	*/
+	for(index_row=0; index_row<period; index_row++){
+		data_val = matrix[index_row][DATA_A_COLUMN];
+		mcorr_add(mcorr, (void*)&data_val);
+		data_val = matrix[index_row][DATA_B_COLUMN];
+		squeue_insert(second_var, (void*)&data_val);
+	}
+	for (index_row=period; index_row<num_rows; index_row++){
+		mcorr_val = mcorr_get(mcorr, second_var);
+		data_val = matrix[index_row-period][MCORR_COLUMN];
+
+		data_val_rounded = (long long int)(data_val * round_mult);
+		mcorr_val_rounded = (long long int)(mcorr_val * round_mult);
+		data_val_rounded /= round_mult;
+		mcorr_val_rounded /= round_mult;
+		
+		if (data_val_rounded != mcorr_val_rounded)
+			printf("\n\ttestMCORRMath: Mismatch values in the row with index %llu (MCORR: %.12f DATA: %.12f)\n",
+				   (unsigned long long int)index_row, mcorr_val, data_val);	
+		ck_assert_double_eq(data_val_rounded, mcorr_val_rounded);
+		
+		data_val = matrix[index_row][DATA_A_COLUMN];
+		mcorr_add(mcorr, (void*)&data_val);
+		data_val = matrix[index_row][DATA_B_COLUMN];
+		squeue_insert(second_var, (void*)&data_val);
+	}
+
+	for (index_row=0; index_row<num_rows; index_row++)
+		free(matrix[index_row]);
+	free(matrix);
+	matrix = NULL;
+
+	table_destruct(&table);
+	mcorr_destruct(&mcorr);
+	squeue_destruct(&second_var);
+}
+	
 Suite* suite_mmcorr(){
     Suite* suite = suite_create("Moving Correlation Suite");
     TCase* tc_mcorr = tcase_create("MCORR");
 
     tcase_add_test(tc_mcorr, testInitDestruct);
 	tcase_add_test(tc_mcorr, testAddGet);
+	tcase_add_test(tc_mcorr, testMCORRMath);
 
 	suite_add_tcase(suite, tc_mcorr); 
     return suite;
